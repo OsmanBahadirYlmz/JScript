ff1e183ec20c4096fb28ab3212594c530f66e4f7
osman bahadir yilmaz <obyilmaz10@gmail.com>
2021-09-24 20:14:22 +0300

    finish

--- 2_projects/4_blackjack/black.js
+++ 2_projects/4_blackjack/black.js
@@ -1,30 +1,89 @@
 let messageEl=document.getElementById("message-el")
 let cardsEl=document.getElementById("cards-el")
 let sumEl=document.getElementById("sum-el")
-let fcard=10
-let scard=6
-let sum=fcard+scard
+
+
+let fcard
+let scard
+let cards=[]
+let sum=0
 let hasblackjack=false
-let isalive=true
+let isalive=false
 let message = ""
 
+let player= {
+    name:"per",
+    chips:142
+}
+
+let playerEl=document.getElementById("player-el")
+playerEl.textContent=player.name + ": $" + player.chips
 
 
 
+function getRandomCard(){
+    let randomnum=Math.floor(Math.random()*13)+1
+    console.log(randomnum)
+    if (randomnum===1) {        
+        return 11
+    }
+    if (randomnum>10) {
+        return 10
+    }
+    else {
+        return randomnum
+    }
+
+    
+}
+
+function startgame(){
+    isalive=true
+    fcard=getRandomCard()
+    scard=getRandomCard()
+    cards=[fcard,scard]
+    sum=fcard+scard
+    console.log(cards)
+    rendergame()
 
 
-function startgame() {
+}
+
+
+
+function rendergame() {
     if (sum<=20){
         message = "Do you want to draw a new card?"
     }else if (sum===21) {
         message="Wohoo! oyu've got Blackjack!"
+        hasblackjack=true
     }else {
         message="you're out of game"
         isalive=false
     }
     console.log(message)
     messageEl.textContent=message
-    cardsEl.textContent=fcard+" "+scard
-    sumEl.textContent="Sum:"+sum
+    cardsEl.textContent="Cards: "
+    for (let i=0; i<cards.length; i++){
+        cardsEl.textContent+=cards[i]+ " "
+    }
     
+    console.log(sum)
+    sumEl.textContent="Sum: "+sum
 }
+
+
+function newcard(){
+    if (isalive===true && hasblackjack===false) {
+        let card =getRandomCard()
+        sum+=card
+        cards.push(card)
+        rendergame()
+    }
+    
+    
+   
+}
+
+
+

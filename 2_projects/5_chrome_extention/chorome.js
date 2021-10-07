120b0c836293a0fb0492b67f872e780ee4ed4fcf
osman bahadir yilmaz <obyilmaz10@gmail.com>
2021-10-07 23:43:35 +0300

    add

---
+++ 2_projects/5_chrome_extention/chorome.js
@@ -0,0 +1,14 @@
+let person= {
+    name:"osman",
+    age:33,
+    country:"TR",
+
+}
+
+
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
+console.log("ab")
